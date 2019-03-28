bf3674e439bc0abd4e446d26c38aeba36886b37b
Alex Perea <apexmd21@gmail.com>
2019-03-28 12:57:13 -0500

    repeated string updated

--- repeated_string.py
+++ repeated_string.py
@@ -1,14 +1,20 @@
-def repeatedString(s, n):
-    total = 0
-    a = 0
+def repeatedString(s,n):
     l = len(s)
+    q, r = divmod(n, l)
+    a = 0
+    # get number of a
     for c in s:
         if c == 'a':
-            a += 1
+            a+=1
+
+    total = a*q
+
+    for x in range(0, r):
+        if( s[x] == 'a' ):
+            total += 1
+
+    return total
     
-    print(a)
 
-
-repeatedString('abcac', 10)
-# abcacabcac
-# abcacabcacab
+repeatedString('a', 100000000)
+#abcac abcac abc
